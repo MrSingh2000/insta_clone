9ff1098b614a110be03ad0f65f669e82f68abd15
Anshuman Singh <singhanshuman1020@gmail.com>
2022-08-23 00:02:26 +0530

    edit loading screen

--- client/src/components/common/Loader.js
+++ client/src/components/common/Loader.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./custom.css";
+import logo from "../../static/logo.png"
 
 function Loader() {
     return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <span className="loader"></span>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <img src={logo} alt="logo" width="100px" />
+            <p className="mt-2 text-xl font-semibold italic">Not Instagram</p>
+            {/* <span className="non-italic">&#128540;</span> */}
+            <span className="loader mt-10"></span>
         </div>
     )
 }
