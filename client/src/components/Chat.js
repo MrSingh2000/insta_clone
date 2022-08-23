ff8422657f7ea94dec5e561325df6aad67f86453
Anshuman Singh <singhanshuman1020@gmail.com>
2022-08-23 11:55:47 +0530

    mobile chat screen done

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -15,7 +15,7 @@ function Chat() {
     const [nowChat, setNowChat] = useState({ friend: "", pic: "", chat: [] });
     const [chatMess, setChatMess] = useState([]);
     const handleClick = (index) => {
-        setNowChat({friend: chats[index].friend, pic: chats[index].pic, chat: chats[index].chat});
+        setNowChat({ friend: chats[index].friend, pic: chats[index].pic, chat: chats[index].chat });
         setChatMess(chats[index].chat);
         setMess("");
     }
@@ -42,21 +42,21 @@ function Chat() {
                     message: message,
                 }]);
             }
-            else{
+            else {
                 console.log("pass");
             }
         })
     }, [nowChat]);
 
-    
+
 
 
     return (
         <div className="flex flex-col items-center h-screen overflow-hidden">
             <DesktopNav />
 
-            <div className="flex lg:w-8/12 w-full justify-center grow" style={{marginTop: '60px'}}>
-                <div className=" grow sm:w-40 text-center">
+            <div className={`flex lg:w-8/12 w-full justify-center grow`} style={{ marginTop: '60px' }}>
+                <div className={`${nowChat.friend ? "hidden sm:block" : ""} grow sm:w-40 text-center`}>
                     <p className="border-l-2 border-r-2">Username</p>
                     <div className="container border-t-2 border-l-2 border-r-2 flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 shadow">
                         <ul className="flex flex-col divide divide-y w-full overflow-auto h-4/6">
@@ -161,6 +161,68 @@ function Chat() {
                     )}
 
                 </div>
+
+                {/* mobile chat*/}
+                {nowChat.friend ? (
+                    <div className={`sm:hidden block mt-15 w-full mx-2`}>
+                        {chatMess.length === 0 ? (
+                            <>
+                                <div className="flex flex-col">
+                                    <div className="flex p-2 items-center sticky border-b-2">
+                                        <img src={nowChat.pic ? nowChat.pic : nopp} alt="pp" className="mx-2 object-cover rounded-full h-10 w-10" />
+                                        <p className="font-semibold">{nowChat.friend}</p>
+                                    </div>
+
+                                    <div className="h-full text-xs italic grow mt-52 flex justify-center items-center flex-col">
+                                        No new chats
+                                    </div>
+                                    <div className="flex flex-col items-center my-5">
+                                        <input onChange={(e) => setMess(e.target.value)} className="ml-2 w-52 border-2 rounded-xl p-1" type="text" placeholder="Say Hi!" />
+                                        <button onClick={() => sendMessage(nowChat.friend)} type="button" className="m-2 py-1 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 w-36 rounded-lg ">
+                                            Send
+                                        </button>
+
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="flex flex-col h-full w-full overflow-hidden">
+
+                                    <div className="flex p-2 items-center sticky border-b-2">
+                                        <img src={nowChat.pic ? nowChat.pic : nopp} alt="pp" className="mx-2 object-cover rounded-full h-10 w-10" />
+                                        <p className="font-semibold">{nowChat.friend}</p>
+                                    </div>
+
+                                    <div className="p-1 overflow-auto h-4/6 block">
+                                        {/* <ul className="h-full overflow-auto"> */}
+                                        {chatMess.map((item, index) => {
+                                            return (
+                                                <p key={index} style={{
+                                                    float: `${item.from === 'a' ? 'right' : 'left'}`,
+                                                    clear: 'both',
+                                                    maxWidth: '20rem'
+                                                }} className={` m-1 p-1 rounded-lg bg-yellow-200`}>
+                                                    {item.message}
+                                                </p>
+                                            )
+                                        })}
+                                        {/* </ul> */}
+                                    </div>
+
+                                    <div className="flex mt-8">
+                                        <input onChange={(e) => setMess(e.target.value)} className="border-2 w-full ml-2 rounded-xl p-1" type="text" placeholder="send message" />
+                                        <button onClick={() => sendMessage(nowChat.friend)} type="button" className="mx-2 py-1 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-13 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                            Send
+                                        </button>
+
+                                    </div>
+
+                                </div>
+                            </>)}
+                    
+                    </div>) : (null)}
+
             </div>
 
             <MobileNav />
---
+++ client/src/components/profile/MobileChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MobileChat() {
+  return (
+    <div>MobileChat</div>
+  )
+}
+
+export default MobileChat
